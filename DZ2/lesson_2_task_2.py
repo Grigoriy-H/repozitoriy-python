19f8651c9f3f8e87b01642e7baa800df7f6f86ec
g.halimanoff <g.halimanoff@gmail.com>
2024-10-21 22:01:22 +0300

    16

--- DZ2/lesson_2_task_2.py
+++ DZ2/lesson_2_task_2.py
@@ -2,7 +2,7 @@ def is_year_leap(year):
      
      return True if (year) % 4 == 0 else False 
 
-num = int(input("ГОД:"))
+num = int(input("ГОД:  "))
 
 result = is_year_leap(num)
 
--- DZ2/lesson_2_task_3.py
+++ DZ2/lesson_2_task_3.py
@@ -1,5 +1,5 @@
 import math
 def square(side):
      return math.ceil(side*side)
-num_side = int(input("Введите число:  "))
+num_side = float(input("Введите число:  "))
 print(f"Площадь квадрата: {square(num_side)}")
